79026a6f30849fdaa49478731edd536c7b42edec
gladythomas <gladychaprath1999@gmail.com>
2025-02-12 08:26:56 +0530

    added files folder in which the tasks are going to be stored and added the files folder to the code so the tasks are going to be stored asa file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const app=express();
-const path=require('path')
+const path=require('path');
+const fs=require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -9,9 +10,15 @@ app.set('view engine', "ejs");
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    fs.readdir(`./files`, function(err,files){
+        
+        res.render('index',{files:files});
+    })
 })
 
+app.post('/create',(req,res)=>{
+    
+})
 
 
 
